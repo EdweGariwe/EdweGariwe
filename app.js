a2dedb742ebae9f5decd7a2d1599d50754e2c05d
EdweGariwe <mbaxtiyor3693@gmail.com>
2024-05-27 15:13:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("brg").addEventListener("click", function () {
+    document.querySelector("header").classList.toggle("open");
+  });
+});
